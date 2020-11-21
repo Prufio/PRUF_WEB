8330e0def00603916bb0b39039136975fe49cf84
James B Smyth <jamesblaydesmyth@gmail.com>
2020-11-21 13:42:13 -0400

    new build

---
+++ web_app/build/precache-manifest.4c0587b14dc76a6304f607d30f73c215.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c8a01d390238ff3f4bcf12ae52b8b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12c3b1f034dc2337fe51",
+    "url": "./static/css/2.40563451.chunk.css"
+  },
+  {
+    "revision": "7ade4045952a0ad7b63f",
+    "url": "./static/css/main.2c64ba9f.chunk.css"
+  },
+  {
+    "revision": "12c3b1f034dc2337fe51",
+    "url": "./static/js/2.29e25703.chunk.js"
+  },
+  {
+    "revision": "ced1f1efbe9e5f919ac62a43235f7472",
+    "url": "./static/js/2.29e25703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ade4045952a0ad7b63f",
+    "url": "./static/js/main.09ae495f.chunk.js"
+  },
+  {
+    "revision": "8e7b9f5a1be431a611a6",
+    "url": "./static/js/runtime-main.17ab6e71.js"
+  },
+  {
+    "revision": "3f7c1b451759e7eeebf05b65afbbebad",
+    "url": "./static/media/BlueBackMobile.3f7c1b45.png"
+  },
+  {
+    "revision": "a09bb5787236abab058fa6ee9db4edb6",
+    "url": "./static/media/Pruf AR (2).a09bb578.png"
+  },
+  {
+    "revision": "a19dfa191f13f71eda93285b76e71575",
+    "url": "./static/media/PrufACAdmin.a19dfa19.png"
+  },
+  {
+    "revision": "7583b219355fcf734eba8b119b2bd071",
+    "url": "./static/media/PrufAssetMinter.7583b219.png"
+  },
+  {
+    "revision": "6cc65a69d3394c672e7add545bc4efe7",
+    "url": "./static/media/PrufBasic.6cc65a69.png"
+  },
+  {
+    "revision": "da338e7b2af616ad5f5c3eea0c2305e6",
+    "url": "./static/media/PrufFaucet.da338e7b.png"
+  },
+  {
+    "revision": "b2768b440862c8d77866aab04c7b33ee",
+    "url": "./static/media/PrufPrintBackground.b2768b44.png"
+  },
+  {
+    "revision": "0e5a531e03f242f7ef459f9168f1a7db",
+    "url": "./static/media/PrufReadOnly.0e5a531e.png"
+  },
+  {
+    "revision": "ca3269e0a4fb89ed0dc93136aeb16700",
+    "url": "./static/media/PrufTokenHolder.ca3269e0.png"
+  },
+  {
+    "revision": "5dbc108566478c10ce2c2639fffb4bf1",
+    "url": "./static/media/PrufTokenMinter.5dbc1085.png"
+  },
+  {
+    "revision": "57cc72921b561f6d52fd0dfc076b3ac5",
+    "url": "./static/media/print.57cc7292.png"
+  }
+]);
