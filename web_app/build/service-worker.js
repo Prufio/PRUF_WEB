bd4e37c2f0847228d8c041334e86f424ed0ae0f0
James B Smyth <jamesblaydesmyth@gmail.com>
2020-11-26 21:04:45 -0400

    rebuild w/ addr serve

--- web_app/build/service-worker.js
+++ web_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.91fd522abf1cc62d642fc3d96576acb3.js"
+  "./precache-manifest.d0dca0fb5ce9a5cc5533a7252bd804fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
