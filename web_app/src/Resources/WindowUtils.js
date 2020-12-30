8d9fe30842e4eb58c7d91f4179b0ba68f4081deb
James B Smyth <jamesblaydesmyth@gmail.com>
2020-12-30 15:24:40 -0400

    More printcard improvements

--- web_app/src/Resources/WindowUtils.js
+++ web_app/src/Resources/WindowUtils.js
@@ -1458,7 +1458,7 @@ function buildWindowUtils() {
             logoImage="https://pruf.io/assets/images/pruf-u-logo-with-border-323x429.png"
           />
         </div>
-        <div className="cardHref">https://app.pruf.io {window.printObj.idxHash}</div>
+        <div className="cardHref">https://app.pruf.io</div>
         <div className="cardDate">{date}</div>
         <div className="printFormContent">
           <img
@@ -1478,6 +1478,7 @@ function buildWindowUtils() {
           <p className="cardNamePrint">Name : {window.printObj.name}</p>
           <p className="cardAcPrint">Asset Class : {window.printObj.assetClassName}</p>
           <h4 className="cardIdxPrint">IDX : {window.printObj.idxHash}</h4>
+          
         </div>
       </div >
     )
