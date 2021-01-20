c1595df1b79f8cc35b3ca9e052af73799cfa2813
James B Smyth <jamesblaydesmyth@gmail.com>
2021-01-20 14:49:25 -0400

    Revert "troubleshooting"
    This reverts commit 18f81acbe684561ad0c0100e1ce67a972fa22da8.

--- web_app/build/service-worker.js
+++ web_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.29dfd211728d1d193f35c2430d940735.js"
+  "/precache-manifest.09af01a1f878818f24205a1eff716b4e.js"
 );
 
 self.addEventListener('message', (event) => {
