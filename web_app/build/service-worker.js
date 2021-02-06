6478b2611d829b14cc68888764fc8187f4c5a2a5
James B Smyth <jamesblaydesmyth@gmail.com>
2021-02-06 16:20:22 -0400

    rebuild reflecting some fixes

--- web_app/build/service-worker.js
+++ web_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6dc4fb0610a10a840b7a3cab86ec41f.js"
+  "/precache-manifest.8160e8b29e997e39c9787e6c294829d9.js"
 );
 
 self.addEventListener('message', (event) => {
