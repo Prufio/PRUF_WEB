583a53dcd2d5ec0d627117d4b47ec6f7b779b1f3
Drake Smyth <drakesmyth@gmail.com>
2021-02-08 17:21:10 -0400

    build

--- web_app/build/service-worker.js
+++ web_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03eb4af7a889edf0f7850b79e0521c17.js"
+  "/precache-manifest.11cfc6ee053d3ccf6e75cb1e1c202c38.js"
 );
 
 self.addEventListener('message', (event) => {
