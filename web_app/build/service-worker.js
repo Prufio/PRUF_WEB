db717fd7be9b6b28d81d62dd3a8a07b00f8dfaea
James B Smyth <jamesblaydesmyth@gmail.com>
2021-02-24 12:57:24 -0400

    build

--- web_app/build/service-worker.js
+++ web_app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01f65fbacfdf023ce8e2b49f8f2ff1e4.js"
+  "/precache-manifest.374cc2f60db310a0a5845574ecc44925.js"
 );
 
 self.addEventListener('message', (event) => {
