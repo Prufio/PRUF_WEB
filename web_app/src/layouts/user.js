4818c64db595902d92a155a3e755e6f1a74023e4
Drake Smyth <drakesmyth@gmail.com>
2021-06-14 12:23:41 -0400

    build

--- web_app/src/layouts/user.js
+++ web_app/src/layouts/user.js
@@ -1809,7 +1809,7 @@ export default function Dashboard(props) {
           <Card>
             <CardHeader color="info" icon>
               <CardIcon className="headerIconBack">
-                <span class="material-icons"> toll </span>
+                <span className="material-icons"> toll </span>
               </CardIcon>
               <h5 className={classes.cardIconTitle}>Split Tokens</h5>
             </CardHeader>
