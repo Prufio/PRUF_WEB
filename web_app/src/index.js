61f9fac171e99851290f320e9777eda681390ab5
James Smyth <jamesblaydesmyth@gmail.com>
2021-10-21 23:31:09 -0400

    rebuild

--- web_app/src/index.js
+++ web_app/src/index.js
@@ -16,16 +16,57 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, RouteProps, Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 import AdminLayout from "layouts/user.js";
+import UAuth from '@uauth/js'
 import { CookiesProvider } from "react-cookie";
 
 import "assets/scss/material-dashboard-pro-react.scss?v=1.9.0";
 
-console.log(window.location.href)
+const uauth = new UAuth({
+  // Client credentials copied from https://unstoppabledomains.com/app-dashboard
+  clientID: "Q2pO03hsT5gMk0IxAacVZoloemjGBzvVEzxaofTHnmA=",
+  clientSecret: "+2Kdmv50Sl3zGxhV2ZnfCuDYmPfpbE5ulPRHPtzIoO4=",
+
+  // Requested scopes.
+  // scope: 'openid email wallet',
+
+  // Redirect Uris copied from https://unstoppabledomains.com/app-dashboard
+  redirectUri: "https://staking.pruf.io/callback",
+  postLogoutRedirectUri: "https://staking.pruf.io/",
+})
+
+const Callback = props => {
+  const [redirectTo, setRedirectTo] = useState()
+
+  useEffect(() => {
+    // Try to exchange authorization code for access and id tokens.
+    uauth
+      .loginCallback()
+      // Successfully logged and cached user in `window.localStorage`
+      .then(response => {
+        console.log('loginCallback ->', response)
+        setRedirectTo('/#/stake')
+      })
+      // Failed to exchange authorization code for token.
+      .catch(error => {
+        console.error('callback error:', error)
+        setRedirectTo('/#/stake')
+      })
+  }, [])
+
+  if (redirectTo) {
+    return <Redirect to={redirectTo} />
+  }
+
+  return <>Loading...</>
+}
 
 ReactDOM.render(
   <CookiesProvider>
+    <BrowserRouter>
+    <Route path="/callback" component={Callback} />
+    </BrowserRouter>
     <HashRouter>
       <Switch>
         <Route path="/stake" component={AdminLayout} />
