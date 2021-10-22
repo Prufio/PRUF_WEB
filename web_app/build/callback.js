a9070f1b9ff2a792a3bd077419a03dbece255155
James Smyth <jamesblaydesmyth@gmail.com>
2021-10-21 22:12:31 -0400

    WIP

--- web_app/build/callback.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-    const { name = 'World' } = req.query;
-    console.log(req.query)
-    res.status(200).send(`Hello ${name}!`);
-    window.location.href="staking.pruf.io"
-  };
